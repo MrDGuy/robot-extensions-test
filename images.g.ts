ab79bf2aba55df59b123da54f1392e5440747660
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-06-21 11:57:31 -0400

    Create images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,97 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Robot Up":return img`
+. . . . . . . . . . . . . . . 
+. . . . . . . a . . . . . . . 
+. . . . . . . a . . . . . . . 
+. . . . . . a a a . . . . . . 
+. . . . . . a a a . . . . . . 
+. . . . . a a a a a . . . . . 
+. . . . . a a a a a . . . . . 
+. . . . a a a a a a a . . . . 
+. . . . a a a a a a a . . . . 
+. . . a a a a a a a a a . . . 
+. . . a a a a a a a a a . . . 
+. . a a a a a a a a a a a . . 
+. . a a a a a a a a a a a . . 
+. a a a a a a a a a a a a a . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Robot Left":return img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . a . . 
+. . . . . . . . . . a a a . . 
+. . . . . . . . a a a a a . . 
+. . . . . . a a a a a a a . . 
+. . . . a a a a a a a a a . . 
+. . a a a a a a a a a a a . . 
+. a a a a a a a a a a a a . . 
+. . a a a a a a a a a a a . . 
+. . . . a a a a a a a a a . . 
+. . . . . . a a a a a a a . . 
+. . . . . . . . a a a a a . . 
+. . . . . . . . . . a a a . . 
+. . . . . . . . . . . . a . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "Robot Down":return img`
+. . . . . . . . . . . . . . . 
+. a a a a a a a a a a a a a . 
+. . a a a a a a a a a a a . . 
+. . a a a a a a a a a a a . . 
+. . . a a a a a a a a a . . . 
+. . . a a a a a a a a a . . . 
+. . . . a a a a a a a . . . . 
+. . . . a a a a a a a . . . . 
+. . . . . a a a a a . . . . . 
+. . . . . a a a a a . . . . . 
+. . . . . . a a a . . . . . . 
+. . . . . . a a a . . . . . . 
+. . . . . . . a . . . . . . . 
+. . . . . . . a . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "Robot Right":return img`
+. . . . . . . . . . . . . . . 
+. a a . . . . . . . . . . . . 
+. a a a a . . . . . . . . . . 
+. a a a a a a . . . . . . . . 
+. a a a a a a a a . . . . . . 
+. a a a a a a a a a a . . . . 
+. a a a a a a a a a a a a . . 
+. a a a a a a a a a a a a a . 
+. a a a a a a a a a a a a . . 
+. a a a a a a a a a a . . . . 
+. a a a a a a a a . . . . . . 
+. a a a a a a . . . . . . . . 
+. a a a a . . . . . . . . . . 
+. a a . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
